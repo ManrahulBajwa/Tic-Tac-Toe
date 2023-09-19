494d23665b9ef5a2e4e0a98086cd296906076329
Manrahul Bajwa <manrahulbajwa@gmail.com>
2023-09-19 06:07:23 +0530

    improved

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -4,7 +4,6 @@ const Box = ({ value, onClick }) => {
             <button onClick={onClick} className='bg-slate-900 flex justify-center items-center p-3 cursor-pointer text-8xl font-semibold hover:text-white/75 min-h-[110px] min-w-[96px] text-white uppercase'>
                 {value}
                 </button>
-            {/* <div className='bg-white rounded-md flex justify-center items-center p-3 cursor-pointer'><BsXLg/></div> */}
         </>
     )
 }
